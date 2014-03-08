8fd1c7614b96e3225142ea33a48a094e11262f42
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-03-07 22:55:02 -0600

    Adding lsyncd to metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name    'rax-wordpress'
 version '0.1.0'
 license 'Apache 2.0'
 
-%w{ apache database firewall iptables-ng mysql varnish wordpress }.each do |cb|
+%w{ apache database firewall iptables-ng lsyncd mysql varnish wordpress }.each do |cb|
   depends cb
 end
 
