fba94af9ac275acd43a6493bb76decc3c29e7fff
Jason Boyles <jason@duncancreek.net>
2014-05-06 17:09:56 -0500

    innodb-buffer-pool config

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name    'rax-wordpress'
 version '0.1.0'
 license 'Apache 2.0'
 
-%w{ apache database firewall iptables-ng lsyncd mysql varnish wordpress }.each do |cb|
+%w{ rax-mysql-tunables apache database firewall iptables-ng lsyncd mysql varnish wordpress }.each do |cb|
   depends cb
 end
 
