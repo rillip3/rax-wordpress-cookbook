b822333dcce53d16aa79e7c8a4fb983e6ad26425
Justin Phelps <linuturk@onitato.com>
2014-11-20 16:58:49 -0600

    these changes are required for the updates to wordpress-single vsftpd

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ version '0.1.0'
 license 'Apache 2.0'
 
 %w{ rax-mysql-tunables apache database firewall iptables-ng lsyncd mysql
-    varnish wordpress memcached }.each do |cb|
+    varnish wordpress memcached vsftpd }.each do |cb|
   depends cb
 end
 
